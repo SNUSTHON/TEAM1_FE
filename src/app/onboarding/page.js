17303b5a02a8b88f48051a7e5fa0db31c22edbce
Younghyo Cho <yhcho0405@kakao.com>
2024-09-08 04:56:25 +0900

    Update page.js

--- src/app/onboarding/page.js
+++ src/app/onboarding/page.js
@@ -1,17 +1,17 @@
 import React from "react";
-import "./onboarding.css";
+import styles from "./onboarding.module.css";
 import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="app">
-      <div className="overlay">
-        <h1>Think wave</h1>
-        <p>
+    <div className={styles.app}>
+      <div className={styles.overlay}>
+        <h1 className={styles.title}>Think wave</h1>
+        <p className={styles.description}>
           ThinkWave는 휴리스틱을 방지하고자 반대 의견 및 주제를 제시하는 확장형
           메모 서비스입니다.
         </p>
-        <Link className="get-started-btn" href="/login">
+        <Link className={styles.getStartedBtn} href="/login">
           Get started
         </Link>
       </div>
